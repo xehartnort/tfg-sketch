fdc6a1590feae481ee6adcf2d8912a57b81290a7
xehartnort <xehartnort@gmail.com>
2019-01-14 18:44:40 +0100

    hacky trick

--- libs/GoL.py
+++ libs/GoL.py
@@ -1,8 +1,11 @@
+#hacky
+import sys, os
+sys.path = [os.path.abspath(os.path.dirname(__file__))] + sys.path
 
 import numpy as np
 import numexpr as ne
 #CUSTOM IMPORT
-from libs.custom_rand import *
+from custom_rand import *
 
 setSeed(-1)
 
