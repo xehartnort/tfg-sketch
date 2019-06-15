ccb674e0434d592170b67743e1ea38dc6eaed95a
xehartnort <xehartnort@gmail.com>
2019-06-15 18:19:11 +0200

    testing new values in order to avoid zero range

--- analyzeData.py
+++ analyzeData.py
@@ -9,14 +9,12 @@ def computeStats(values):
     mean = np.sum(values) / length
     squared_mean = np.sum(values*values) / length
     values_std = math.sqrt((squared_mean - mean**2) / length)
-    accumulated = np.array([np.sum(values[:i])/i for i in range(length-100, length+1)])
+    accumulated = np.array([np.sum(values[:i])/i for i in range(int(length*0.75), length+1)])
     accumulated_length = len(accumulated)
     i = 0
-    #print(len(accumulated[i:]))
     w, p_value = shapiro(accumulated[i:])
     while p_value < 0.05 and (accumulated_length-i) > 20:
         i += 10
-        #print(len(accumulated[i:]))
         w, p_value = shapiro(accumulated[i:])
     return (mean, values_std, p_value)
 
